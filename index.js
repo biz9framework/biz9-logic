aba06ec0404185d8a301b98b8159be4e1030fbaa
Certified CoderZ <certifiedcoderz@gmail.com>
2025-04-28 19:35:18 -0400

    good

--- index.js
+++ index.js
@@ -5,13 +5,18 @@ License GNU General Public License v3.0
 Description: BiZ9 Framework: Logic-JS
 */
 const { get_new_item_main,get_data_config_main,get_cloud_url_main,get_biz_item_main,get_cloud_filter_obj_main,get_title_url_main } = require('./main');
-const {Log,Test,Str,DateTime} = require('biz9-utility');
+const { Log,Test,Str,DateTime } = require('biz9-utility');
+
 class Message {
+
 	static SUCCESS="Update Success";
 	static LOGIN_GOOD="Login Success";
 	static LOGIN_BAD="Login Incorrect";
+
 }
+
 class DataType {
+
 	static get_title = (data_type) => {
 		if(!data_type){
 			return "";
@@ -19,6 +24,7 @@ class DataType {
 			return String(Str.get_title(data_type.replaceAll('_',' ').replaceAll('dt','').replace('biz',''))).trim();
 		}
 	}
+
 	static DT_ADMIN='admin_biz';
 	static DT_BLANK='blank_biz';
 	static DT_BUSINESS='business_biz';
@@ -43,12 +49,15 @@ class DataType {
 	static DT_VIDEO='video_biz';
 }
 class DataItem {
+
 	static get_new = (data_type,id) => {
 		return get_new_item_main(data_type,id);
 	};
+
 	static get_biz = (biz9_config,item,options)=>{
 		return get_biz_item_main(biz9_config,item,options);
 	}
+
 	static get_biz_by_list = (biz9_config,list,options)=>{
 		let r_list = [];
 		for(let a=0;a<list.length;a++){
@@ -56,76 +65,107 @@ class DataItem {
 		}
 		return r_list;
 	}
+
 }
 class BiZ_Url {
+
 	static get_item=(biz9_config,data_type,id)=>{
 		let action_url="main/biz_item/get/"+data_type+"/"+id ;
 		return get_cloud_url_main(biz9_config.APP_TITLE_ID,biz9_config.URL,action_url,null);
 	};
+
 	static delete_item=(biz9_config,data_type,id)=>{
 		let action_url= "main/biz_item/delete/"+data_type+"/"+id ;
 		return get_cloud_url_main(biz9_config.APP_TITLE_ID,biz9_config.URL,action_url,null);
 	};
+
 	static get_full_item=(biz9_config,data_type,id)=>{
 		let action_url= "main/biz_item/get_full/"+data_type+"/"+id;
 		return get_cloud_url_main(biz9_config.APP_TITLE_ID,biz9_config.URL,action_url,null);
 	};
+
+	static get_page=(biz9_config,data_type,title_url)=>{
+		let action_url= "main/biz_item/get_page/"+data_type+"/"+title_url;
+		return get_cloud_url_main(biz9_config.APP_TITLE_ID,biz9_config.URL,action_url,null);
+	};
+
+	static get_template=(biz9_config,title_url)=>{
+		let action_url= "main/biz_item/get_template/"+title_url;
+		return get_cloud_url_main(biz9_config.APP_TITLE_ID,biz9_config.URL,action_url,null);
+	};
+
 }
 class Url {
+
 	static get = (biz9_config,action_url,params)=>{
 		return get_cloud_url_main(biz9_config.APP_TITLE_ID,biz9_config.URL,action_url,params);
 	}
+
 	static connect = (biz9_config) => {
 		let action_url= "main/test/connect/";
 		return get_cloud_url_main(biz9_config.APP_TITLE_ID,biz9_config.URL,action_url,null);
 	};
+
 	static upload_file = (biz9_config,data_type,id) => {
 		let action_url= "main/crud/update/"+data_type + "/" + id;
 		return get_cloud_url_main(biz9_config.APP_TITLE_ID,biz9_config.URL,action_url,null);
 	};
+
 	static update_item = (biz9_config,data_type,id) => {
 		let action_url= "main/crud/update/"+data_type + "/" + id;
 		return get_cloud_url_main(biz9_config.APP_TITLE_ID,biz9_config.URL,action_url,null);
 	};
+
 	static get_item = (biz9_config,data_type,id) => {
 		let action_url= "main/crud/get/"+data_type + "/" + id;
 		return get_cloud_url_main(biz9_config.APP_TITLE_ID,biz9_config.URL,action_url,null);
 	};
+
 	static delete_item = (biz9_config,data_type,id) => {
 		let action_url= "main/crud/delete/"+data_type + "/" + id;
 		return get_cloud_url_main(biz9_config.APP_TITLE_ID,biz9_config.URL,action_url,null);
 	};
+
 	static get_list = (biz9_config,data_type) => {
 		let action_url= "main/crud/get_list/"+data_type;
 		return get_cloud_url_main(biz9_config.APP_TITLE_ID,biz9_config.URL,action_url,null);
 	};
+
 	static update_list = (biz9_config,data_type) => {
 		let action_url= "main/crud/update_list/"+data_type;
 		return get_cloud_url_main(biz9_config.APP_TITLE_ID,biz9_config.URL,action_url,null);
 	};
+
 	static delete_list = (biz9_config,data_type) => {
 		let action_url= "main/crud/delete_list/"+data_type;
 		return get_cloud_url_main(biz9_config.APP_TITLE_ID,biz9_config.URL,action_url,null);
 	};
+
 	static delete_biz_item = (biz9_config,data_type,id) => {
 		let action_url= "main/biz_item/delete/"+data_type + "/" + id;
 		return get_cloud_url_main(biz9_config.APP_TITLE_ID,biz9_config.URL,action_url,null);
 	};
+
 	static get_biz_list = (biz9_config,data_type) => {
 		let action_url= "main/biz_item/get_list/"+data_type;
 		return get_cloud_url_main(biz9_config.APP_TITLE_ID,biz9_config.URL,action_url,null);
 	};
+
 	static delete_biz_list = (biz9_config,data_type) => {
 		let action_url= "main/biz_item/delete_list/"+data_type;
 		return get_cloud_url_main(biz9_config.APP_TITLE_ID,biz9_config.URL,action_url,null);
 	};
+
 }
 class Obj {
+
 	static get_filter = (biz9_config,data_type,filter,sort_by,page_current,page_size)=>{
 		return get_cloud_filter_obj_main(data_type,filter,sort_by,page_current,page_size);
 	}
+
 };
 class Cat {
+
 	static get_category_drop_down_list_by_list = (cat_list) => {
 		let category_list = [];
 		for(let a=0;a<cat_list.length;a++){
@@ -133,6 +173,7 @@ class Cat {
 		}
 		return category_list;
 	};
+
 	static get_category_list = () => {
 		return [
 			{ value: DataType.BLOG_POST, label: "Blog Posts" },
@@ -144,6 +185,7 @@ class Cat {
 			{ value: DataType.DT_TEMPLATE, label: "Template" }
 		];
 	};
+
 	static get_title_by_type = (data_type) => {
 		switch (data_type) {
 			case DataType.DT_EVENT:
@@ -183,8 +225,10 @@ class Cat {
 				return "Blank";
 		}
 	}
+
 };
 class CMS {
+
 	static Tab_Edit_Title_General = 'general';
 	static Tab_Edit_Title_Photo = 'photo';
 	static Tab_Edit_Title_List = 'list';
@@ -194,6 +238,7 @@ class CMS {
 	static Tab_Edit_Title_Video = 'video';
 	static Tab_Edit_Title_Note = 'note';
 	static get_new_query_item_by_item = (item) => {
+
 		return {
 			id: item.id ? item.id : 0,
 			data_type:item.data_type ? item.data_type : DataType.DT_BLANK,
@@ -205,6 +250,7 @@ class CMS {
 			top_data_type:item.top_data_type ? item.top_data_type : DataType.DT_BLANK
 		}
 	};
+
 	static get_new_query_item = (item,parent_item,top_item) => {
 		return {
 			id: item.id ? item.id : 0,
@@ -216,27 +262,35 @@ class CMS {
 			top_data_type:top_item.data_type ? top_item.data_type : DataType.DT_BLANK
 		}
 	};
+
 	static get_query_itemz_by_query = (item) => {
 		return [];
 	};
+
 	static get_query_item_by_page = (item) => {
 		return { id:item.id ? item.id : 0, data_type:item.data_type ? item.data_type : DataType.DT_BLANK };
 	};
+
 	static get_query_parent_item_by_page = (parent_item) => {
 		return { id:parent_item.id ? parent_item.id : 0, data_type:parent_item.data_type ? parent_item.data_type : DataType.DT_BLANK };
 	};
+
 	static get_query_top_item_by_page = (top_item) => {
 		return { id:top_item.id ? top_item.id : 0, data_type:top_item.data_type ? top_item.data_type : DataType.DT_BLANK };
 	};
+
 	static get_query_item_by_query = (query) => {
 		return { id:query.get('id') ? query.get('id') : 0, data_type:query.get('data_type') ? query.get('data_type') : DataType.DT_BLANK } ;
 	};
+
 	static get_query_parent_item_by_query = (query) => {
 		return { id:query.get('parent_id') ? query.get('parent_id') : 0, data_type:query.get('parent_data_type') ? query.get('parent_data_type') : DataType.DT_BLANK } ;
+
 	};
 	static get_query_top_item_by_query = (query) => {
 		return { id:query.get('top_id') ? query.get('top_id') : 0, data_type:query.get('top_data_type') ? query.get('top_data_type') : DataType.DT_BLANK } ;
 	};
+
 	static get_sub_page_title = (title) => {
 		switch(title)
 		{
@@ -266,6 +320,7 @@ class CMS {
 				break;
 		}
 	}
+
 	static get_page_url = (url,tab_title,item,parent_item,top_item,parms) => {
 		let r_url="?tab_title="+tab_title
 			+"&id="+item.id
@@ -284,8 +339,10 @@ class CMS {
 		}
 		return url+r_url;
 	}
+
 }
 class Stock {
+
 	static get_event_stock_list = () => {
 		const r_list=
 			[
@@ -296,6 +353,7 @@ class Stock {
 			];
 		return r_list;
 	};
+
 	static get_event_stock_by_value = (stock_val) => {
 		switch(stock_val)
 		{
@@ -313,6 +371,7 @@ class Stock {
 				break;
 		}
 	};
+
 	static get_service_stock_list = () => {
 		const r_list=
 			[
@@ -322,6 +381,7 @@ class Stock {
 			];
 		return r_list;
 	};
+
 	static get_service_stock_by_value = (stock_val) => {
 		switch(stock_val)
 		{
@@ -336,6 +396,7 @@ class Stock {
 				break;
 		}
 	};
+
 	static get_product_stock_list = () => {
 		const r_list=
 			[
@@ -346,6 +407,7 @@ class Stock {
 			];
 		return r_list;
 	};
+
 	static get_product_stock_by_value = (stock_val) => {
 		switch(stock_val)
 		{
@@ -369,6 +431,7 @@ class Stock {
 
 }
 class Schedule {
+
 	static get_start_date_time = (item) =>{
 		if(!item.date){
 			item.date = new Date();
@@ -381,6 +444,7 @@ class Schedule {
 		item.start_date_time = DateTime.get_full_date_time_by_date_time(item.date,item.time);
 		return item;
 	}
+
 	static get_start_date_time_by_list = (list) =>{
 		for(let a=0;a<list.length;a++){
 			if(!list[a].date){
@@ -395,8 +459,10 @@ class Schedule {
 		}
 		return list;
 	}
+
 };
 class Storage {
+
 	static KEY_ADMIN="key_admin";
 	static KEY_USER="key_user";
 	static KEY_BUSINESS="key_business";
@@ -404,15 +470,19 @@ class Storage {
 	static get = (window,key) => {
 		return JSON.parse(window.localStorage.getItem(key));
 	}
+
 	static set = (window,key,obj) => {
 		window.localStorage.setItem(key,JSON.stringify(obj));
 	}
+
 	static remove = (window,key) =>{
 		window.localStorage.removeItem(key);
 	}
+
 	static clear = (window) =>{
 		window.localStorage.clear();
 	}
+
 }
 module.exports = {
 	BiZ_Url,
