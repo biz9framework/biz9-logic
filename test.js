db9eccf95db4226e022394d4212e533fe2ea6399
Certified CoderZ <certifiedcoderz@gmail.com>
2025-06-17 11:15:03 -0400

    good

--- test.js
+++ test.js
@@ -147,7 +147,7 @@ describe("connect", () => {
                 /* --PAGE--START */
                 //let page = Page_Logic.get_test()
                 //let page = Page_Logic.get_test("Page "+Number.get_id())
-                let page = Page_Logic.get_test("Page "+Number.get_id(),{get_value:true,value_count:5,get_section:true,section_count:2})
+                let page = Page_Logic.get_test("Page "+Number.get_id(),{get_value:true,value_count:5,get_section:true,section_count:20})
                 Log.w("page",page);
                 Log.w("page_section_1",page.section_1);
                 Log.w("page_section_6",page.section_6.items);
