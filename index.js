c2996305d75bbb033929daea297140edd3824b31
Certified CoderZ <certifiedcoderz@gmail.com>
2025-09-24 00:46:47 -0400

    good

--- index.js
+++ index.js
@@ -1838,7 +1838,7 @@ class Url_Logic {
 	static URL_GALLERY_SEARCH="gallery/search";
 	//image
 	static URL_IMAGE_POST="main/image/post";
-	static URL_IMAGE_POST="main/image/post_cdn";
+	static URL_IMAGE_CDN_POST="main/image/post_cdn";
 	//order
 	static URL_ORDER_DELETE="item/delete_order";
 	static URL_ORDER_GET="item/get_order";
